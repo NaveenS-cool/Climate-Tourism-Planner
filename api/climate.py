fc6d421a56559c430916eaee7a6e244d4a8c99e5
Aravind A R <aravindar9778@gmail.com>
2026-06-20 13:19:19 +0530

    Update 2

--- api/climate.py
+++ api/climate.py
@@ -1,6 +1,7 @@
 
 import requests
 from  .locator import get_coords
+from datetime import date
 
 def get_climate(lat, long):
 
@@ -23,18 +24,22 @@ def get_climate(lat, long):
         "past_days": 7,
         "timezone": "auto"
     }
-
+   
     data = requests.get(url, params=params).json()
 
     return data["daily"]
 
 location = "Coorg, Karnataka"
+today=date.today().isoformat()
 
 lat, long = get_coords(location)
 
 daily = get_climate(lat, long)
+current={}
 
 for i in range(len(daily["time"])):
+    if daily["time"][i]==today:
+        current={0:today,1:daily["temperature_2m_mean"][i],2: daily["relative_humidity_2m_mean"][i],3:daily["precipitation_sum"][i]} 
     print("Date:", daily["time"][i])
     print("Max Temp:", daily["temperature_2m_max"][i])
     print("Min Temp:", daily["temperature_2m_min"][i])
@@ -46,3 +51,6 @@ for i in range(len(daily["time"])):
     print("Sunshine Duration:", daily["sunshine_duration"][i])
     print("Max Wind Speed:", daily["wind_speed_10m_max"][i])
     print("-" * 40)
+
+for i in range(4):
+    print(current[i])
